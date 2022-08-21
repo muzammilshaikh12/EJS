d89ef476d760b562d6ae5863f5fbdbab933e7319
muzammilshaikh12 <muzammildoc@gmail.com>
2022-08-21 20:44:25 +0530

    alt

---
+++ DOM-1/dom.js
@@ -0,0 +1,7 @@
+const a = document.getElementById('header-title');
+a.innerHTML="Non Item Lister"
+const b=document.getElementById('main-header')
+b.style.borderBottom="3px solid black"
+
+const c= document.querySelector(".title")
+c.style.color="green"
